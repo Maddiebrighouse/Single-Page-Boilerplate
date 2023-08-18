ac8d784411aee02d3d1d1504dfc9ea0fcc4d2cc0
Madeleine-Brighouse <madeleinebrighouse@gmail.com>
2023-08-18 19:26:54 +1200

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black min-h-screen flex justify-center">
+      <div className="relative flex-col my-auto">
+        <h1 className="text-4xl text-white font-bold underline">
+          Vite + TypeScript + Tailwind
+        </h1>
+        <div className="text-2xl text-white">
+          Single Page Application (SPA) Template
+        </div>
+        <div className="text-sm text-white">With a few themes and layouts</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
