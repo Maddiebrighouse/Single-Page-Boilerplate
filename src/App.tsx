92432a6eb9f1000ce44ec4cdca32026a2023fc4c
Madeleine-Brighouse <madeleinebrighouse@gmail.com>
2023-08-27 12:55:16 +1200

    tailwind set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,60 @@
+import React from "react";
+import config from "../site-config.json";
+
+const LandingPage = () => {
+  return (
+    <div className="bg-black min-h-screen flex snap-center">
+      <div className="relative flex flex-col m-auto">
+        <img
+          src={config.images[0].src}
+          alt={config.images[0].alt}
+          className="invert"
+        />
+        <h1 className="text-4xl text-white font-bold underline mx-auto">
+          {config.title}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+const FirstLayout = () => {
+  return (
+    <div className="min-h-screen flex snap-center">
+      <div className="relative flex flex-col m-auto">
+        <img src={config.images[0].src} alt={config.images[0].alt} />
+        <h1 className="text-4xl text-black font-bold underline mx-auto">
+          {config.title}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+const SecondLayout = () => {
+  return (
+    <div className="bg-black min-h-screen flex justify-center snap-center">
+      <div className="relative flex m-auto">
+        <h1 className="text-4xl text-white font-bold underline mx-auto">
+          {config.description}
+        </h1>
+        <div className="text-white">{config.title}</div>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className="bg-black min-h-screen flex justify-center">
-      <div className="relative flex-col my-auto">
-        <h1 className="text-4xl text-white font-bold underline">
-          Vite + TypeScript + Tailwind
-        </h1>
-        <div className="text-2xl text-white">
-          Single Page Application (SPA) Template
-        </div>
-        <div className="text-sm text-white">With a few themes and layouts</div>
+    <div className="snap-mandatory snap-y">
+      <div className="snap-center">
+        <LandingPage />
+      </div>
+      <div className="snap-center">
+        <FirstLayout />
+      </div>
+      <div className="snap-center">
+        <SecondLayout />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        strobe: {
+          '50%': { background: 'black', color: 'white' }
+        },
+        
+        animation: {
+          strobe: 'strobe 200ms steps(1,end) infinite',
+        }
+      }
+    },
   },
   plugins: [],
 }
